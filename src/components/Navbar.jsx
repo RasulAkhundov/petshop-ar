c79a6d77ebc0864045c1f5593ca2156b65eae1ca
“Resulaxundov” <resul.axundov2002@mail.ru>
2022-06-05 06:03:05 +0400

    icon chnages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const Navbar = ({ user }) => {
@@ -11,9 +11,9 @@ const Navbar = ({ user }) => {
    const [userMe, setuserMe] = useState([]);
 
    useEffect(() => {
-      if(user) {
+      if (user) {
          setuserMe(user);
-         if(user.carts !== undefined) {
+         if (user.carts !== undefined) {
             setUserMeCarts(user.carts);
          }
       }
@@ -25,19 +25,19 @@ const Navbar = ({ user }) => {
       const userId = user._id;
 
       const removeCart = await axios.delete(`${process.env.REACT_APP_API_URL}/api/delete-cart/${cartId}/user-id/${userId}`)
-      .then(res => {
-         window.location.reload(true);
-         return res.data
-      })
-      .catch(err => {
-         if(err) {
-            console.log('error from removing cart of user' + err);
-         }
-      })
+         .then(res => {
+            window.location.reload(true);
+            return res.data
+         })
+         .catch(err => {
+            if (err) {
+               console.log('error from removing cart of user' + err);
+            }
+         })
    }
 
    const changeHandler = (e) => {
-      if(window.scrollY > 120) {
+      if (window.scrollY > 120) {
          console.log()
          setHeaderFixed(true);
       } else {
@@ -48,7 +48,7 @@ const Navbar = ({ user }) => {
    window.addEventListener('scroll', changeHandler);
 
    const menuToggle = () => {
-      if(toggle === false) {
+      if (toggle === false) {
          setToggle(true)
       } else {
          setToggle(false)
@@ -56,7 +56,7 @@ const Navbar = ({ user }) => {
    }
 
    const shoppingCartBtn = () => {
-      if(shoppingCartToggle === false) {
+      if (shoppingCartToggle === false) {
          setShoppingCartToggle(true)
       } else {
          setShoppingCartToggle(false)
@@ -74,10 +74,10 @@ const Navbar = ({ user }) => {
       })
    })
 
-  return (
-    <div className={headerFixed === true ? 'navigation-bar navigation-bar-fixed' : 'navigation-bar'}>
-       <div className="container">
-         <div className="row align-items-center" style={{position: "relative", transition: "all 300ms ease"}}>
+   return (
+      <div className={headerFixed === true ? 'navigation-bar navigation-bar-fixed' : 'navigation-bar'}>
+         <div className="container">
+            <div className="row align-items-center" style={{ position: "relative", transition: "all 300ms ease" }}>
                <div className="col-6 col-lg-2 d-flex align-items-center">
                   <div className="logo">
                      <a href='/'><img src={require('../assets/logo.jpg')} alt="" /></a>
@@ -98,67 +98,75 @@ const Navbar = ({ user }) => {
                </div>
                <div className="col-6 col-lg-2 d-flex align-items-center justify-content-end">
                   <div className="search_login_cart_wrapper">
-                     <div className="search">
-                        <i className="fa-solid fa-magnifying-glass"></i>
+                     <div className="search d-flex align-items-center">
+                        <span className="material-symbols-outlined">
+                           search
+                        </span>
                      </div>
-                     <div className="login">
+                     <div className="login d-flex align-items-center">
                         {
                            userMe._id ?
-                           <a href='/'><i className="fa-solid fa-arrow-right-from-bracket" onClick={logOut}></i></a>
-                           :
-                           <a href='/account/login'><i className="fa-regular fa-user"></i></a>
+                              <a className='d-flex align-items-center' href='/'><span className="material-symbols-outlined" onClick={logOut}>
+                                 logout
+                              </span></a>
+                              :
+                              <a className='d-flex align-items-center' href='/account/login'><span className="material-symbols-outlined">
+                                 person
+                              </span></a>
                         }
                      </div>
-                     <div className="cart">
-                        <i className="fa-solid fa-bag-shopping" onClick={shoppingCartBtn}></i>
+                     <div className="cart d-flex flex-column justify-content-center">
+                        <span className="material-symbols-outlined" onClick={shoppingCartBtn}>
+                           shopping_bag
+                        </span>
                         <div className="cart_adding_number" id="cart_adding_number" onClick={shoppingCartBtn}>
                            {userMeCarts.length > 0 ? userMeCarts.length : 0}
                         </div>
                         <div className={shoppingCartToggle === false ? 'shopping_cart_content' : 'shopping_cart_content shopping-cart-active'}>
-                        {
-                           userMeCarts.length > 0 ?
-                           <div className="shopping_cart_cover">
-                              {userMeCarts.map((carts, i) => (
-                                 <li className='single_shopping_cart mb-3' key={i}>
-                                    <div className="shopping_cart_img">
-                                       <a href={`/products/${carts.productID}`}><img src={user && require('../uploads/product-images/' + carts.image)} alt="" /></a>
+                           {
+                              userMeCarts.length > 0 ?
+                                 <div className="shopping_cart_cover">
+                                    {userMeCarts.map((carts, i) => (
+                                       <li className='single_shopping_cart mb-3' key={i}>
+                                          <div className="shopping_cart_img">
+                                             <a href={`/products/${carts.productID}`}><img src={user && require('../uploads/product-images/' + carts.image)} alt="" /></a>
+                                          </div>
+                                          <div className="shopping_cart_title">
+                                             <a href={`/products/${carts.productID}`}><h4>{carts.title} - {carts.size} / {carts.color} / {carts.material}</h4></a>
+                                             <h6>Qty: {carts.number}</h6>
+                                             <span className='money'>${carts.price}.00</span>
+                                          </div>
+                                          <div className="shopping_cart_close">
+                                             <i className="fa-regular fa-circle-xmark" id='delete_cart_element' onClick={() => deleteCartElement(carts._id)}></i>
+                                          </div>
+                                       </li>
+                                    ))}
+                                    <div className="shopping_cart_total">
+                                       <h4>
+                                          Total:
+                                          <span style={{ color: "#F6AB4B" }}>${totalPrice}.00</span>
+                                       </h4>
                                     </div>
-                                    <div className="shopping_cart_title">
-                                       <a href={`/products/${carts.productID}`}><h4>{carts.title} - {carts.size} / {carts.color} / {carts.material}</h4></a>
-                                       <h6>Qty: {carts.number}</h6>
-                                       <span className='money'>${carts.price}.00</span>
+                                    <div className="shopping_cart_btn">
+                                       <a href='/cart'><button type='button'>VIEW CART</button></a>
+                                       <a href='/checkouts/:id'><button type='button'>CHECKOUT</button></a>
                                     </div>
-                                    <div className="shopping_cart_close">
-                                       <i className="fa-regular fa-circle-xmark" id='delete_cart_element' onClick={() => deleteCartElement(carts._id)}></i>
-                                    </div>
-                                 </li>
-                              ))}
-                              <div className="shopping_cart_total">
-                                 <h4>
-                                    Total: 
-                                    <span style={{ color: "#F6AB4B" }}>${totalPrice}.00</span>
-                                 </h4>
-                              </div>
-                              <div className="shopping_cart_btn">
-                                 <a href='/cart'><button type='button'>VIEW CART</button></a>
-                                 <a href='/checkouts/:id'><button type='button'>CHECKOUT</button></a>
-                              </div>
-                           </div>
-                           :
-                           <p>Cart cannot find</p>
-                        }
+                                 </div>
+                                 :
+                                 <p>Cart cannot find</p>
+                           }
                         </div>
                      </div>
                      <div className="menu-toggle" onClick={menuToggle}>
                         {
                            toggle === false ?
-                           <div className="line-wrapper">
-                              <div className="single-line line"></div>
-                              <div className="single-line line"></div>
-                              <div className="single-line line"></div>
-                           </div>
-                           :
-                           <span>X</span>
+                              <div className="line-wrapper">
+                                 <div className="single-line line"></div>
+                                 <div className="single-line line"></div>
+                                 <div className="single-line line"></div>
+                              </div>
+                              :
+                              <span>X</span>
                         }
                      </div>
                   </div>
@@ -176,10 +184,10 @@ const Navbar = ({ user }) => {
                      }
                   </ul>
                </div>
-          </div>
-       </div>
-    </div>
-  )
+            </div>
+         </div>
+      </div>
+   )
 }
 
 export default Navbar
